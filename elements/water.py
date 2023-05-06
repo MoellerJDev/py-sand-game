8051ead60335c811ba72a11444f2cd6844d8eaae
Joshua <josh.t.m@icloud.com>
2023-05-06 14:49:12 -0500

    Add water delay tick-based behavior

--- elements/water.py
+++ elements/water.py
@@ -1,30 +1,23 @@
+import pygame
 from element import Element
 from elements.sand import Sand
-import pygame
+import settings
+
 
 class Water(Element):
     def __init__(self, x, y):
         super().__init__(x, y)
         self.color = (0, 0, 255)  # Blue color
+        self.tick_counter = 0  # Tick counter for delay
 
     def update(self, grid):
-        # If the space below this element is empty or is Sand, move down
-        if self.y < len(grid) - 1 and (grid[self.y + 1][self.x] is None or isinstance(grid[self.y + 1][self.x], Sand)):
-            grid[self.y][self.x] = None
-            self.y += 1
-            grid[self.y][self.x] = self
-        # If can't move down but can move left, move left
-        elif self.x > 0 and grid[self.y][self.x - 1] is None:
-            grid[self.y][self.x] = None
-            self.x -= 1
-            grid[self.y][self.x] = self
-        # If can't move down or left but can move right, move right
-        elif self.x < len(grid[0]) - 1 and grid[self.y][self.x + 1] is None:
-            grid[self.y][self.x] = None
-            self.x += 1
-            grid[self.y][self.x] = self
+        # Delay the movement of water by checking the tick counter
+        if self.tick_counter < settings.WATER_DELAY:
+            self.tick_counter += 1
+            return
 
-    def update(self, grid):
+        self.tick_counter = 0  # Reset the tick counter
+
         # If the space below this element is empty or is Sand, move down
         if self.y < len(grid) - 1 and (grid[self.y + 1][self.x] is None or isinstance(grid[self.y + 1][self.x], Sand)):
             grid[self.y][self.x] = None
